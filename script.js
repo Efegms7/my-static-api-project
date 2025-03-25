b6c12e26c73b0890b8aec5463d57b682e9a5950a
Efegms7 <hllefegms@gmail.com>
2025-03-25 20:15:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+function fetchDog() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("dogImage").src = data.message;
+    })
+    .catch(error => console.log("Hata:", error));
+}
